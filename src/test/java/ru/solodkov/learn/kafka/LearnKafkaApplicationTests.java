d326a7ea3b65542d6ca36bcc2803e3c9e9b3e138
Valenstain <svs.laboratory@gmail.com>
2024-11-16 12:29:25 +0300

    Create spring boot

---
+++ src/test/java/ru/solodkov/learn/kafka/LearnKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.solodkov.learn.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
